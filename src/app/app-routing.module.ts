d3f3dcb004164a136bd2f283059a8d138b95eaf8
= <=>
2024-07-01 18:04:39 +0200

    New song page design.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ArtistComponent } from './pages/artist/artist.component';
 import { CompanyComponent } from './pages/company/company.component';
+import { NewSongComponent } from './pages/new-song/new-song.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'artist', component: ArtistComponent },
   { path: 'company', component: CompanyComponent },
+  { path: 'newsong', component: NewSongComponent },
 ];
 
 @NgModule({
